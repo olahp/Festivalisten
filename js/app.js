57924640426c8e6c318214608e2ae8ae0b1d30b1
Michael Enger <michael.enger@webtraffic.no>
2014-01-29 13:32:10 +0100

    Add filtering by location

--- js/app.js
+++ js/app.js
@@ -5,14 +5,30 @@
 	 * Initialize the app - setup events and such.
 	 */
 	var Festivalnorge = function(festivals) {
-		var festivalElement = $(".festivals");
+		var festivalsElement = $(".festivals"),
+			filters = {
+				location: ""
+			},
+			filterLocation = $(".filters .filter-location");
 
+		this.festivals = festivals;
+		this.dates = [];
 		this.genres = [];
-		this.createFestivalList(festivals, festivalElement);
-		this.resizeCells(festivalElement);
+		this.locations = [];
+
+		this.createFestivalList(festivals, festivalsElement);
+		this.resizeCells(festivalsElement);
+
+		// Filters
+		this.locations.sort(function(a, b) {
+			return a.toLowerCase().localeCompare(b.toLowerCase());
+		});
+		$.each(this.locations, function() {
+			filterLocation.append("<option value=\"" + this + "\">" + this + "</option>");
+		});
 
 		// Setup isotope
-		festivalElement.isotope({
+		festivalsElement.isotope({
 			itemSelector: ".item",
 			layoutMode: "fitRows"
 		});
@@ -20,7 +36,11 @@
 		// Events
 		var self = this;
 		$(window).on("resize", function() {
-			self.resizeCells(festivalElement);
+			self.resizeCells(festivalsElement);
+		});
+		filterLocation.on("change", function() {
+			filters.location = this.value;
+			self.filterFestivalItems(filters, festivalsElement);
 		});
 	};
 
@@ -34,11 +54,22 @@
 		for (var i = 0; i < festivals.length; i++) {
 			festival = festivals[i];
 
-			if (typeof festival.date == "object" && festival.date.length) {
-				if (festival.date.length > 2) {
-					festival.date = festival.date.join(", ");
+			if (festival.date) {
+				if (typeof festival.date == "object" && festival.date.length) {
+					for (var d = 0; d < festival.date.length; d++) {
+						if (this.dates.indexOf(festival.date[d]) == -1) {
+							this.dates.push(festival.date[d]);
+						}
+					}
+					if (festival.date.length > 2) {
+						festival.date = festival.date.join(", ");
+					} else {
+						festival.date = festival.date[0] + " - " + festival.date[1];
+					}
 				} else {
-					festival.date = festival.date[0] + " - " + festival.date[1];
+					if (this.dates.indexOf(festival.date) == -1) {
+						this.dates.push(festival.date);
+					}
 				}
 			}
 
@@ -49,10 +80,33 @@
 				}
 			}
 
+			if (festival.location) {
+				if (this.locations.indexOf(festival.location) == -1) {
+					this.locations.push(festival.location);
+				}
+			}
+
 			element.append(Mustache.render(template, festival));
 		}
 
 		element.find(".loading").remove();
+	};
+
+	/**
+	 * Filter the festival items using isotope.
+	 */
+	Festivalnorge.prototype.filterFestivalItems = function(filters, element) {
+		var filter = "";
+
+		for (var key in filters) {
+			if (filters[key] != "") {
+				filter += "[data-" + key + "=" + filters[key] + "]";
+			}
+		}
+
+		element.isotope({
+			filter: filter != "" ? filter : "*"
+		});
 	};
 
 	/**
